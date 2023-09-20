23279cb8f79bb553de05371225cb6e6a22d4a2e7
lithrow <486115192@qq.com>
2023-09-20 10:56:00 +0800

    sss修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
         <el-input :prefix-icon="User" v-model="formData.username"></el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input  :prefix-icon="Lock" v-model="formData.password" show-password></el-input>
+        <el-input :prefix-icon="Lock" v-model="formData.password" show-password></el-input>
       </el-form-item>
       <el-form-item>
         <div class="btn-box">
@@ -15,6 +15,7 @@
         </div>
       </el-form-item>
     </el-form>
+    <p>我搜索</p>
   </div>
 </template>
 
@@ -24,15 +25,15 @@ import { ref, reactive } from 'vue'
 import { userLogin } from '../utils/api'
 import { useRouter } from 'vue-router'
 const router = useRouter()
-const formData= reactive({
+const formData = reactive({
   username: '',
   password: ''
 })
 
 const rules = reactive({
   username: [
-    { required: true,  message: '用户名不能为空', trigger: 'blur' },
-    { min: 3, max: 10, message: '账号在3～10个字符之间', trigger: 'blur'}
+    { required: true, message: '用户名不能为空', trigger: 'blur' },
+    { min: 3, max: 10, message: '账号在3～10个字符之间', trigger: 'blur' }
   ],
   password: [
     { required: true, min: 3, max: 10, message: '密码不能为空', trigger: 'blur' }
@@ -43,10 +44,10 @@ const instance = ref()
 
 const submit = () => {
   instance.value.validate(valid => {
-    if(valid) {
+    if (valid) {
       userLogin(formData).then(res => {
         // console.log(res)
-        if(res.meta.status === 200) {
+        if (res.meta.status === 200) {
           localStorage.setItem('token', res.data.token)
           router.push('/about')
         }
@@ -69,19 +70,22 @@ const reset = () => {
   align-items: center;
   justify-content: center;
 }
+
 .form {
   padding: 20px;
   width: 450px;
   background-color: #fff;
   border-radius: 8px;
-  box-shadow: 0 0 10px #fff; 
+  box-shadow: 0 0 10px #fff;
 }
+
 .title {
   text-align: center;
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 20px;
 }
+
 .btn-box {
   display: flex;
   justify-content: flex-end;
